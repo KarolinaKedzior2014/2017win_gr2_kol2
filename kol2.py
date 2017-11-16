0b3d18b06590c05e152e36809dfecd1a04cb7505
[Karolina Kedzior] <[Karolina.Kedzior@fis.agh.edu.pl]>
2017-11-16 17:07:55 +0100

    First solution

--- kol2.py
+++ kol2.py
@@ -13,3 +13,53 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+import sys
+import math
+import random
+
+class StudentOrganizer:
+	def __init__(self, list_of_students):
+		self.students = []
+	def get_avg_class_score(self, class_name):
+		avg = 0.0
+		i = 0
+		for student in self.students:
+			if student.class_name == class_name:
+				avg = avg + student.score
+				i = i+1
+		return float(avg/i)
+	def get_total_avg_score(self):
+		avg = 0.0
+		i = 0
+		for student in self.students:
+			avg = avg + sum(student.scores)			
+			i = i+ 1
+		return avg
+
+	
+class Student:
+	def __init__(self,name, surname, class_name):
+		self.name = name
+		self.surname = surname
+		self.class_name = class_name
+		self.scores = []
+		self.attendance = { }
+	def add_score(self, new_score):
+		self.scores.append(new_score)
+	def set_if_present(self, date, is_present):
+		self.attendance[date] = is_present
+
+
+if __name__ == "__main__":
+	my_students_list = [Student( "Tom" , "A" , "1A"),Student( "Tom" , "w" , "1A"),Student( "Tom" , "A" , "1B"), Student( "Tom" , "e" , "1B"),Student( "Tom" , "D" , "1B"),Student( "Tom" , "C" , "1C"),Student( "Tom" , "b" , "1C")]
+	for student in my_students_list:
+		student.add_score( random.randint(2,6) )
+		student.set_if_present( "11.11.2017",  random.randint(0,2) )
+	student_organizer = StudentOrganizer(my_students_list)
+	print (student_organizer.get_total_avg_score() )
+	
+
+
+
+	
+
